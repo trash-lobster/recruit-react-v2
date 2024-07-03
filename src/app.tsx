7b96d5cf8ac52bde92ba1b833359f51453cd77ed
Fernando Zhu <fernandoisadev@gmail.com>
2024-07-03 19:07:49 +1200

    Setup unit tests toolchain

--- src/app.tsx
+++ src/app.tsx
@@ -2,5 +2,5 @@ import React, { FC } from "react";
 import * as styles from "./app.module.scss";
 
 export const App = () => {
-  return <div className={styles.demo}>Hello</div>;
+  return <h1 className={styles.demo}>Welcome to your technical test!</h1>;
 };
