0026a44aec8ed98df59826ab00e9371b26f02941
Vincent Sit <vincentsit00@ymail.com>
2025-06-30 17:27:38 +1200

    Moved spec file into separate folder for clarity

--- test/app.spec.tsx
+++ test/app.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { App } from "./app";
+import { App } from "../src/app";
 
 describe("Dummy test", () => {
   it("should display welcome text", () => {
