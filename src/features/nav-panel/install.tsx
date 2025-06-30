a8a06e8e1e37bd5365e6d8596651b958fd94e81d
Vincent Sit <vincentsit00@ymail.com>
2025-06-30 17:42:26 +1200

    Handle state pass down

--- src/features/nav-panel/install.tsx
+++ src/features/nav-panel/install.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { NavPanel as InnerNavPanel } from './navbar';
 
-export function installNavPanel() {
+export function installNavPanel( 
+    isNavMenuActive: boolean, 
+    setIsNavMenuActive: React.Dispatch<React.SetStateAction<boolean>> 
+) {
+    const onIconClick = () => setIsNavMenuActive(!isNavMenuActive);
+
     const NavPanel = () => {
         return (
-            <InnerNavPanel title='' isNavMenuActive={true}/>
+            <InnerNavPanel 
+                title={isNavMenuActive ? "Menu" : "Register card form"}
+                isNavMenuActive={isNavMenuActive}
+                handleClickButton={ onIconClick }
+            />
         )
     }
 
--- src/features/nav-panel/navbar.tsx
+++ src/features/nav-panel/navbar.tsx
@@ -4,13 +4,14 @@ import { ArrowIcon, MenuIcon } from '../icons/icons';
 interface NavPanelProps {
     title: string;
     isNavMenuActive: boolean;
+    handleClickButton: () => void;
 }
 
-export function NavPanel({ title, isNavMenuActive } : NavPanelProps) {
+export function NavPanel({ title, isNavMenuActive, handleClickButton } : NavPanelProps) {
     return (
         <nav>
-            <div className='nav-panel-icon'>
-                {isNavMenuActive ? <ArrowIcon /> : <MenuIcon />}
+            <div className='nav-panel-icon' onClick={ handleClickButton }>
+                { isNavMenuActive ? <ArrowIcon /> : <MenuIcon /> }
             </div>
             <div className='nav-panel-title'>{ title }</div>
         </nav>
